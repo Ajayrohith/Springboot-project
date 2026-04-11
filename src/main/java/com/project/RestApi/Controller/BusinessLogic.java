32fe0443d22d9c7dbeaf53cc3db02ffa20b574bd
Ajay Rohith G <ajay.g@newgensoft.com>
2026-04-11 15:31:30 +0530

    Uploaded from ofc laptop 11th april

--- src/main/java/com/project/RestApi/Controller/BusinessLogic.java
+++ src/main/java/com/project/RestApi/Controller/BusinessLogic.java
@@ -4,12 +4,15 @@ package com.project.RestApi.Controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project.RestApi.Components.CreateUserRequest;
+import com.project.RestApi.Components.*;
 
 import jakarta.annotation.PostConstruct;
 
@@ -39,6 +42,14 @@ public class BusinessLogic {
             throw new CustomException("The id is greater than the number of student ");
         }
         return listobj.get(id);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleexception(CustomException e)
+    {
+        ErrorResponse obj = new ErrorResponse(-1, e.getMessage(), System.currentTimeMillis());
+
+        return new ResponseEntity<>(obj,HttpStatus.BAD_REQUEST);
     }
 
 }
